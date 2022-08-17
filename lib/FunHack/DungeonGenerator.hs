1692207eeb70934d2e4b269ba2463f44a752c344
Aura Kelloniemi <kaura.dev@sange.fi>
2022-08-17 19:12:32 +0300

    DungeonGenerator: add level map manipulation functions
    Add {read,write,modify}LevelMap functions for accessing the level map.
    
    Also expand the stub level generator a bit.

--- lib/FunHack/DungeonGenerator.hs
+++ lib/FunHack/DungeonGenerator.hs
@@ -10,8 +10,18 @@ module FunHack.DungeonGenerator
         makeLevelMap,
         showLevelMap,
 
+        -- ** Read and update level maps
+        readLevelMap,
+        writeLevelMap,
+        modifyLevelMap,
+
         -- * Dungeon generation
-        makeNetHackLevel
+        makeNetHackLevel,
+
+        -- ** Rooms
+        Room,
+        makeRoom,
+        makeRooms
     ) where
 
 import Data.Text qualified as T
@@ -67,6 +77,50 @@ showLevelMap = MV.foldM' foldLine T.empty
         Doorway -> '+'
         Corridor -> ','
 
+-- | Read a cell at a given point from a leel map.
+--
+-- This function fails with an exception if the location is invalid.
+readLevelMap
+    :: (MV.PrimMonad m)
+    => Point3D -- ^ The cell location
+    -> LevelMap (MV.PrimState m) -- ^ The map to read from
+    -> m LevelCell
+readLevelMap point lmap
+    = let x = fromIntegral point.x
+          y = fromIntegral point.y
+      in MV.read lmap y >>= flip MV.read x
+
+-- | Write a new value to a level map at a given point.
+--
+-- This function fails with an exception, if hte location is invalid.
+writeLevelMap
+    :: (MV.PrimMonad m)
+    => Point3D -- ^ The cell location
+    -> LevelCell -- ^ The new cell value
+    -> LevelMap (MV.PrimState m) -- ^ The map to read from
+    -> m ()
+writeLevelMap point value lmap
+    = let x = fromIntegral point.x
+          y = fromIntegral point.y
+      in MV.read lmap y >>= \vec -> MV.write vec x value
+
+-- | Modify a cell in a level map at a given point.
+--
+-- This function fails with an exception, if hte location is invalid.
+modifyLevelMap
+    :: (MV.PrimMonad m)
+    => Point3D -- ^ The cell location
+    -> (LevelCell -> LevelCell) -- ^ A function called with the old value and which produces a new cell value
+    -> LevelMap (MV.PrimState m) -- ^ The map to read from
+    -> m ()
+modifyLevelMap point f lmap
+    = let x = fromIntegral point.x
+          y = fromIntegral point.y
+      in do
+    vec <- MV.read lmap y
+    val <- MV.read vec x
+    MV.write vec x $! f val
+
 -- | Make a NetHack-style level map with the given dimensions.
 makeNetHackLevel
     :: (MV.PrimMonad m)
@@ -74,5 +128,65 @@ makeNetHackLevel
     -> Distance -- ^ Height of the level map
     -> m (LevelMap (MV.PrimState m))
 makeNetHackLevel width height = do
+    -- Create level map
     level <- makeLevelMap width height
+
+    -- Create rooms
+    let levelRect = makeRectangle (Point3D 0 0 0) width height
+    rooms <- makeRooms level levelRect
     pure $! level
+
+-- | A room descriptor contains all necessary information to place a room on a
+-- map and to connect it to other rooms on the level.
+data Room = Room {
+    -- | Occupied area
+    rectangle :: Rectangle,
+
+    -- | Whether the room has been connected to other rooms
+    connections :: Int,
+
+    -- | List of door positions
+    doorways :: [Point3D]
+    } deriving stock (Eq, Show)
+
+-- | Make a room and place it on a level map. This modifies the map in place
+-- and returns a room description.
+makeRoom
+    :: (MV.PrimMonad m)
+    => LevelMap (MV.PrimState m) -- ^ Map to put the room on
+    -> Rectangle -- ^ The rectangle that the rooms should occupy
+    -> m Room
+makeRoom level rect = do
+    pure $! Room rect 0 []
+
+-- | Make rooms on a level map and return them as a list.
+--
+-- The first argument is the level map to be used for placing the rooms. The
+-- map is modified in place. The room areas are filled with floor symbols and
+-- wallified with the wall symbols.
+--
+-- The second argument specifies the rectangle within which the rooms are
+-- created. If the rectangle is bigger than the allocated map, bad things will
+-- happen.
+makeRooms
+    :: forall m. (MV.PrimMonad m)
+    => LevelMap (MV.PrimState m) -- ^ Map of the level (this is odified in place)
+    -> Rectangle -- ^ The bounding rectangle for the created rooms
+    -> m [Room]
+makeRooms level rect = do
+    go [] [rect]
+  where
+    -- | Recursively create rooms in one of the provided rectangles. Already
+    -- created rooms are passed as the first argument. Finally all created
+    -- rooms are returned.
+    go :: [Room] -> [Rectangle] -> m [Room]
+    go rooms [] = pure $! rooms
+    go rooms (r : rects) = do
+        let roomRect = makeRectangle (Point3D r.origin.x r.origin.y r.origin.z) ((r.width `div` 2) - 1) ((r.height `div` 2) - 1)
+        room <- makeRoom level roomRect
+
+        let rectsAround = case splitRectangleAround r roomRect of
+                              Just rs -> rs
+                              Nothing -> error "Bad rectangle splitting"
+            rects' = rectsAround <> rects
+        pure $! (room : rooms)
