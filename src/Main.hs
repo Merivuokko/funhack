42d0ca169165c6135d9bd9e897a91f6b4e7e17ab
Aura Kelloniemi <kaura.dev@sange.fi>
2022-08-24 15:29:34 +0300

    Begin transition to using the Effectful library.

--- src/Main.hs
+++ src/Main.hs
@@ -3,9 +3,14 @@
 module Main (main) where
 
 import Data.Text.IO qualified as TIO
+import Effectful
+import Effectful.Prim
 
 import FunHack.DungeonGenerator
 
 -- | Main entry point of funhack.
 main :: IO ()
-main = makeNetHackLevel 40 20 >>= showLevelMap >>= TIO.putStr
+main = runEff $! runPrim
+       $! makeNetHackLevel 40 20
+       >>= showLevelMap
+       >>= liftIO . TIO.putStr
