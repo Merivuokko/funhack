dbb4663e7732a361ef0f3807f0fcc93e988f5425
Aura Kelloniemi <kaura.dev@sange.fi>
2023-07-27 20:48:23 +0300

    main: Convert to 3D boxes

--- src/Main.hs
+++ src/Main.hs
@@ -7,10 +7,11 @@ import Effectful
 import Effectful.Prim
 
 import FunHack.DungeonGenerator
+import FunHack.Geometry
 
 -- | Main entry point of funhack.
 main :: IO ()
 main = runEff $! runPrim
-       $! makeNetHackLevel 40 20
+       $! makeNetHackLevel (makeBox (Point 0 0 0) 40 20 1)
        >>= showLevelMap
        >>= liftIO . TIO.putStr
