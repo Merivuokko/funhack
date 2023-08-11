54eb65f72fc7d5d91aee0a7c0f492cd9d6dbc33a
Aura Kelloniemi <kaura.dev@sange.fi>
2023-08-11 14:47:32 +0300

    Add a test suite place holder.
    No real tests have been implemented.

---
+++ test/Main.hs
@@ -0,0 +1,16 @@
+-- |
+-- Module      : Main
+-- Description : Test suite main module
+-- Copyright   : Copyright (C) 2023 Aura Kelloniemi
+-- License     : GPL-3
+-- Maintainer  : kaura.dev@sange.fi
+-- Stability   : experimental
+-- Portability : GHC
+
+
+-- This is a test suite place holder. There are no real tests.
+
+module Main (main) where
+
+main :: IO ()
+main = putStrLn "No tests implemented!"
